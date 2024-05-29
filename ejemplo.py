c3695f1d79fab68f7c0320cbc48158664dffcb86
pancho <fran.hernandezm@duocuc.cl>
2024-05-29 09:22:42 -0400

    cree mi diccionario

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,8 @@
 # Ejercicio diccionario de hoy: usuarios
+diccionario = {
+    "ident": 1,
+    "nombre": "Marriott",
+    "direccion": "Avenida Bernando O'higgins 1123",
+    "funcionando": True,
+    "valor_habitacion": 100000
+}
